c88699b0124b09dd0c91d8b63690e4872392b70e
Alexander Caravaca <acaravac@crsjopgp5813.global.publicisgroupe.net>
2016-10-10 14:36:09 -0600

    enter your num

--- app/pages/authentication/authentication.js
+++ app/pages/authentication/authentication.js
@@ -10,6 +10,17 @@ APP.authentication = (function(){
            validateZipCode();
         });
 
+        $( "#zipcode" ).keydown(function(e) {
+            var key   = e.keyCode ? e.keyCode : e.which;
+
+            if (!( [8, 9, 13, 27, 46, 110, 190].indexOf(key) !== -1 ||
+                    (key == 65 && ( e.ctrlKey || e.metaKey  ) ) ||
+                    (key >= 35 && key <= 40) ||
+                    (key >= 48 && key <= 57 && !(e.shiftKey || e.altKey)) ||
+                    (key >= 96 && key <= 105)
+                )) e.preventDefault();
+        });
+
     };
 
     var init = function() {
